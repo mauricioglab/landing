24d777cf5cf1bf068b7f154d2dbd4066e59dc81a
mauricioglab <tu-email@mauricioglab.com>
2025-09-06 03:02:34 -0300

    Fix: Corregir configuración de base URL para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mauricioglab.github.io',
-  base: '/mauricio-landing',
-  output: 'static',
-  build: {
-    assets: 'assets'
-  }
+  base: '/landing',
+  output: 'static'
 });
